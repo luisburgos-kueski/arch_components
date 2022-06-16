798502a6fe246bcc812499fe1252c3b97083d62e
Luis Burgos <arturo.burgos@kueski.com>
2022-06-15 19:48:42 -0500

    feat(PRODUCT-12283): add getxcontroller view implementation

---
+++ lib/home/controller.dart
@@ -0,0 +1,41 @@
+import 'package:get/get.dart';
+
+import 'view_data_model.dart';
+
+class HomeController extends GetxController {
+  RxList<MerchantViewData> merchantData = RxList();
+  Rx<HomeStatus> status = HomeStatus.initial.obs;
+
+  Future<void> onLoadMerchantsEvent() async {
+    status(HomeStatus.loading);
+
+    /// TODO: Call repository or use case
+    await Future.delayed(const Duration(milliseconds: 2100));
+
+    status(HomeStatus.success);
+    merchantData.addAll(
+      [
+        const MerchantViewData(id: 'xiaomi', name: 'Xiaomi'),
+        const MerchantViewData(id: 'doto', name: 'Tío Doto'),
+        const MerchantViewData(id: 'meibi', name: 'Meibi'),
+        const MerchantViewData(id: 'gaia', name: 'GAIA Design'),
+      ],
+    );
+  }
+
+  Future<void> onClearMerchantsEvent() async {
+    status(HomeStatus.loading);
+
+    /// TODO: Call repository or use case
+    await Future.delayed(const Duration(milliseconds: 1500));
+
+    status(HomeStatus.success);
+    merchantData.clear();
+  }
+
+  Future<void> onNavigateToMerchantDetailEvent(MerchantViewData data) async {
+    final String merchantId = data.id;
+
+    Get.toNamed('/home/$merchantId');
+  }
+}
---
+++ lib/home/controller_view.dart
@@ -0,0 +1,39 @@
+import 'package:arch_components/home/controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'components.dart';
+import 'view_data_model.dart';
+import 'view_template.dart';
+
+class HomeControllerView extends StatelessWidget {
+  const HomeControllerView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(HomeController());
+
+    return Obx(() {
+      return HomeViewTemplate(
+        tag: 'getx',
+        isLoading: controller.status() == HomeStatus.loading,
+        displayFailure: controller.status() == HomeStatus.failure,
+        merchantsList: MerchantsList(
+          items: controller.merchantData(),
+          onGoToMerchantDetail: (merchantData) {
+            controller.onNavigateToMerchantDetailEvent(merchantData);
+          },
+        ),
+        failureViewBuilder: () => const Center(
+          child: Text('TODO: Handle error'),
+        ),
+        onLoadMerchants: () {
+          controller.onLoadMerchantsEvent();
+        },
+        onClearMerchants: () {
+          controller.onClearMerchantsEvent();
+        },
+      );
+    });
+  }
+}
