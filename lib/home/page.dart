4211b75fb50be0fec3648b0d5d824debe04959e9
Luis Burgos <arturo.burgos@kueski.com>
2022-06-15 19:49:17 -0500

    feat(PRODUCT-12283): use feature toggle to swap bloc and controller

--- lib/home/page.dart
+++ lib/home/page.dart
@@ -1,4 +1,6 @@
 import 'package:arch_components/home/bloc.dart';
+import 'package:arch_components/home/controller_view.dart';
+import 'package:arch_components/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -11,11 +13,15 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocProvider(
-        create: (_) => HomeBloc(),
-        child: const HomeView(),
-      ),
-    );
+    if (TempStaticFeatureToggles.useBloc) {
+      return Scaffold(
+        body: BlocProvider(
+          create: (_) => HomeBloc(),
+          child: const HomeView(),
+        ),
+      );
+    }
+
+    return const Scaffold(body: HomeControllerView());
   }
 }
