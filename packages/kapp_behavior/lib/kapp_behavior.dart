629020ade5382ec9a3154d02b79c44f978ea5b26
Luis Burgos <arturo.burgos@kueski.com>
2022-06-16 17:34:32 -0500

    feat(PRODUCT-12283): add kapp_behavior test module

---
+++ packages/kapp_behavior/lib/kapp_behavior.dart
@@ -0,0 +1,27 @@
+library kapp_behavior;
+
+import 'dart:developer' as dev;
+
+/// TODO: Evaluate use InMemoryStore
+/// TODO: Evaluate define AppBehaviorEvent class
+/// TODO: Should the API call be made from here?
+class KAppBehavior {
+  KAppBehavior._();
+  static final KAppBehavior _instance = KAppBehavior._();
+
+  factory KAppBehavior() => _instance;
+
+  Future<void> logEvent({
+    required String name,
+    Map<String, Object?>? parameters,
+  }) async {
+    const String kReservedPrefix = 'kapp_behavior_';
+
+    if (name.startsWith(kReservedPrefix)) {
+      throw ArgumentError.value(name, 'name',
+          'Prefix "$kReservedPrefix" is reserved and cannot be used.');
+    }
+
+    dev.log('[$name, ${DateTime.now()}, $parameters]');
+  }
+}
