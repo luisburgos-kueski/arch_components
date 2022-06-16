f2e8ff6c7915570b31ab698a5f05190576c79da3
Luis Burgos <arturo.burgos@kueski.com>
2022-06-16 17:35:55 -0500

    feat(PRODUCT-12283): add helper method to build lists

--- lib/home/shared/view_data_model.dart
+++ lib/home/shared/view_data_model.dart
@@ -1,3 +1,5 @@
+import 'package:merchants_data/merchant_model.dart';
+
 class MerchantViewData {
   const MerchantViewData({
     required this.id,
@@ -6,6 +8,15 @@ class MerchantViewData {
 
   final String id;
   final String name;
+
+  static List<MerchantViewData> listFrom(List<Merchant> data) => data
+      .map(
+        (merchant) => MerchantViewData(
+          id: merchant.id,
+          name: merchant.name,
+        ),
+      )
+      .toList();
 }
 
 enum HomeStatus { initial, loading, success, failure }
