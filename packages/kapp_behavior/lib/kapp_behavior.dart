767dbe408088154e3b5e1e05b0de4c7d75699720
Luis Burgos <arturo.burgos@kueski.com>
2022-06-20 22:58:32 -0500

    feat(PRODUCT-12283): add app behavior event base class

--- packages/kapp_behavior/lib/kapp_behavior.dart
+++ packages/kapp_behavior/lib/kapp_behavior.dart
@@ -11,6 +11,10 @@ class KAppBehavior {
 
   factory KAppBehavior() => _instance;
 
+  Future<void> log(KAppBehaviorEvent event) {
+    return logEvent(name: event.name, parameters: event.parameters);
+  }
+
   Future<void> logEvent({
     required String name,
     Map<String, Object?>? parameters,
@@ -24,4 +28,18 @@ class KAppBehavior {
 
     dev.log('[$kReservedPrefix$name, ${DateTime.now()}, $parameters]');
   }
+}
+
+abstract class KAppBehaviorEvent {
+  KAppBehaviorEvent({
+    required this.name,
+    this.parameters,
+  });
+
+  KAppBehaviorEvent.named(String aName)
+      : name = aName,
+        parameters = null;
+
+  final String name;
+  final Map<String, Object?>? parameters;
 }
