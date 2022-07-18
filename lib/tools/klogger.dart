adbeb9fe4a4e6dc27096dbd7e3fd830489be36a1
Luis Burgos <arturo.burgos@kueski.com>
2022-07-18 11:17:44 -0500

    chore(CONSUMER-2): remove legacy code notes

--- lib/tools/klogger.dart
+++ lib/tools/klogger.dart
@@ -6,31 +6,3 @@ abstract class KLogger {
     dev.log(message, name: name);
   }
 }
-
-/// KAppBehaviorEvent { name, timestamp, params }
-///
-/// Merchant Item Tapped
-/// Screen  - opened, closed
-/// Login, SignUp
-///
-///
-/// implements ---
-/// Merchant Item Tapped implements
-///
-/// KScreenEvent implements KAppEvent
-/// registerEvent Custom - setCurrentScreen (route, screenName)
-///
-/// Analytics ->
-/// if (event is KScreen) set current
-/// else registerEvent
-///
-/// Data Sources
-/// registerEvent
-///
-/// init (route observer, observers events)
-///
-/// notifier () event observer -> event
-///
-/// Observer Events ->
-/// Sources -> bloc, route observer, widget -> merchants tapped
-///
