b387ee13e58814402dbe37db02c3fe3a016f7f1c
Luis Burgos <arturo.burgos@kueski.com>
2022-07-19 13:42:08 -0500

    fix(CONSUMER-2): remove wrong prefix on event name
    (cherry picked from commit b0f2a18dea21e4d7648c84d0426463b7f4176639)

--- packages/home_feature/lib/domain/domain_events.dart
+++ packages/home_feature/lib/domain/domain_events.dart
@@ -17,14 +17,14 @@ abstract class HomeUseCaseEvent implements KAppBehaviorUseCaseEvent {
 
 class OnMerchantListCleared extends HomeUseCaseEvent {
   @override
-  final String name = 'on_merchant_list_cleared';
+  final String name = 'merchant_list_cleared';
 
   OnMerchantListCleared() : super();
 }
 
 class OnMerchantListLoaded extends HomeUseCaseEvent {
   @override
-  final String name = 'on_merchant_list_loaded';
+  final String name = 'merchant_list_loaded';
 
   @override
   Map<String, dynamic>? get params => {
